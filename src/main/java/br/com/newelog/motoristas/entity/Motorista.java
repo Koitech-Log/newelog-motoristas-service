0c78896970e32c3ce5691e435a6dec2d0afabff9
JoãoCavalcante <joaocavalcante377@gmail.com>
2026-09-11 22:29:39 -0300

    KOI-42 feat: atualização de dependencias, criação da entidade motorista e do repository

---
+++ src/main/java/br/com/newelog/motoristas/entity/Motorista.java
@@ -0,0 +1,39 @@
+package br.com.newelog.motoristas.entity ;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter 
+@Setter 
+@AllArgsConstructor
+@NoArgsConstructor 
+@Builder 
+@Table(name="motorista")
+@Entity 
+
+public class Motorista {
+    @Id 
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "cpf_cnpj", nullable = false, unique = true)
+    private String cpfCnpj;
+    
+
+    
+}
---
+++ src/main/java/br/com/newelog/motoristas/repository/MotoristaRepository.java
@@ -0,0 +1,12 @@
+package br.com.newelog.motoristas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.newelog.motoristas.entity.Motorista;
+
+public interface MotoristaRepository extends JpaRepository<Motorista, Integer>{
+
+
+
+    
+}
