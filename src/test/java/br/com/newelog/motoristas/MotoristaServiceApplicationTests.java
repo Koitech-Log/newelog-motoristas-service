fbfd206ea14dec0721f12ecf4c0a3f339ab921bd
joao-luis-0 <joaoluis14.carvalho@gmail.com>
2026-09-23 10:46:36 -0300

    fix: corrigindo testes do spring boot

--- src/test/java/br/com/newelog/motoristas/MotoristaServiceApplicationTests.java
+++ src/test/java/br/com/newelog/motoristas/MotoristaServiceApplicationTests.java
@@ -2,12 +2,17 @@ package br.com.newelog.motoristas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * Garante que o contexto Spring sobe corretamente, incluindo a migration V1
+ * do Flyway contra o banco H2 do perfil de teste.
+ */
 @SpringBootTest
-class NewelogServiceTemplateApplicationTests {
+@ActiveProfiles("test")
+class MotoristasServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
